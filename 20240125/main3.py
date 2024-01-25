3ce8cf9f5c718eaedc0d369cfbcf5759a8992491
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2024-01-25 19:37:45 +0200

    ДЗ done

---
+++ 20240125/main3.py
@@ -0,0 +1,3 @@
+names = ["Vanya", "Volodya", "Alena", "Semen", "Vadim", "Anya", "Anna", "Jeka", "Alex"]
+ages = [77, 23, 37, 7, 11, 13, 31, 18, 8]
+people = dict(zip(names, ages))
---
+++ 20240125/main5.py
@@ -0,0 +1,5 @@
+names = ["Анна", "Петро", "Марія"]
+grades = [90, 85, 95]
+students_grades = dict(zip(names, grades))
+
+print(students_grades)
