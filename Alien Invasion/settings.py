2043e303728d07010c5b47de3749686fb78ce83d
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2024-03-29 15:09:56 +0200

    .

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -1,9 +1,15 @@
 import pygame
 
-
 class Settings:
     def __init__(self):
-        self.window_width = 1200
-        self.window_height = 800
-        self.window_name = "Alien Invasion"
+        self.bg_color = (60, 47, 86)
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.ship_speed = 0.5
 
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_icon(pygame.image.load("images/icon.jpeg"))
